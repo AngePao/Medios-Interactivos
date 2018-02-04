2a023221738aa528c43a6dcc30e745d1a85fdda2
AngePao <35973461+AngePao@users.noreply.github.com>
2018-02-04 16:18:17 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,26 @@
+function setup() { 
+  createCanvas(400, 400); 
+   background(249,176,94,20)
+	
+		strokeWeight (3);
+	line( 1,1, 400, 1 )
+	
+	strokeWeight (1);
+		fill(102,78,155)
+	ellipse( 65, 150, 120,120)
+	
+strokeWeight (2);
+	line( 0,0, 180, 400)
+	line(0,150, 320,150)
+	
+	
+strokeWeight (1);
+	fill(119,115,115,220)
+	ellipse(400,300,300,300)
+	
+	fill(255,209,0,100)
+	triangle(360,0,200,170, 450,230)
+	
+	strokeWeight (2);
+  line(20,370, 300, 370) 
+}
